e59bd732f3f0ee2d179c394a8f288f52a4b0ec49
Srdjan <srdjan2288@hotmail.com>
2020-07-13 15:15:10 +0200

    Design loader for the application

---
+++ src/common/loader.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { View, Animated, Easing, Text } from "react-native";
+import Logo from "./../assets/images/yoda.png";
+
+class Loader extends Component {
+  constructor(props) {
+    super(props);
+    this.springValue = new Animated.Value(0.3);
+  }
+
+  componentDidMount() {
+    this.spring();
+  }
+
+  spring() {
+    this.springValue.setValue(0.3);
+    Animated.spring(this.springValue, {
+      toValue: 1,
+      friction: 1,
+      useNativeDriver: true,
+    }).start((event) => {
+      if (event.finished) {
+        this.spring();
+      }
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loaderImage}>
+          <Animated.Image
+            style={{
+              transform: [{ scale: this.springValue }],
+              width: 100,
+              height: 100,
+              resizeMode: "contain",
+            }}
+            source={Logo}
+          />
+        </View>
+        <Text style={styles.text}>The Force will be with you soon...</Text>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loaderImage: {
+    borderColor: "#98139C",
+    width: 120,
+    height: 120,
+    padding: 10,
+  },
+  text: {
+    fontSize: 14,
+
+    marginTop: 20,
+  },
+};
+
+export default Loader;
