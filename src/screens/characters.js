9737df78edb0f9fc22945b29e33ea7d4bb5cf2bd
Srdjan <srdjan2288@hotmail.com>
2020-07-13 20:51:10 +0200

    Fetch characters from server

--- src/screens/characters.js
+++ src/screens/characters.js
@@ -1,20 +1,70 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { LOCALHOST, PORT } from "./../../config";
+import Loader from "./../common/loader";
+import StatusMessage from "./../common/statusMessage";
+import { Messages } from "./../helper/constants";
 
-export default class Home extends Component {
+const axios = require("axios");
+
+export default class Characters extends Component {
   constructor() {
     super();
     this.state = {
+      title: "",
+      error: false,
+      loading: true,
+      characters: [],
     };
   }
 
+  async componentDidMount() {
+    const { title } = this.props.route.params;
+    this.setState({ title });
+    this.getCharacters(title);
+  }
+
+  getCharacters = async (title) => {
+    try {
+      let path = `http://${LOCALHOST}:${PORT}/api/v1/starWars/charactersForMovie/?title=${title}`;
+      this.setState({ path });
+      await axios
+        .get(path)
+        .then((response) => {
+          this.setState({ characters: response.data.body });
+          this.setState({ loading: false });
+        })
+        .catch((error) => {
+          this.setState({ error: true });
+          this.setState({ loading: false });
+        });
+    } catch (error) {
+      this.setState({ error: true });
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
+    let { loading, error } = this.state;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>
-          Characters
-        </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {error && (
+          <StatusMessage
+            value={Messages.ERROR}
+            text="We are having problem connecting to mothership. Please try again later"
+            textStyle={styles.text}
+          />
+        )}
+        {loading && <Loader />}
       </View>
     );
   }
 }
+
+const styles = {
+  text: {
+    color: "#000000",
+    textAlign: "center",
+    fontFamily: "Starjedi",
+  },
+};
